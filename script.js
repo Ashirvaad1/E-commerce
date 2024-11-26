71a010719938c6393a6b6e52eda7b45ecf0e49bb
Ashirvaad Verma <ashirvaadverma2000@zohomail.in>
2024-11-26 21:56:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyADJuT7UDOxpu4gaV6aRpOCzsBPBb0O368",
-  authDomain: "e-commerce-6015d.firebaseapp.com",
-  projectId: "e-commerce-6015d",
-  storageBucket: "e-commerce-6015d.firebasestorage.app",
-  messagingSenderId: "996362215179",
-  appId: "1:996362215179:web:9a28af932292d45d9f0c0e",
-  measurementId: "G-80XSTM8F3N"
+    apiKey: "AIzaSyADJuT7UDOxpu4gaV6aRpOCzsBPBb0O368",
+    authDomain: "e-commerce-6015d.firebaseapp.com",
+    projectId: "e-commerce-6015d",
+    storageBucket: "e-commerce-6015d.firebasestorage.app",
+    messagingSenderId: "996362215179",
+    appId: "1:996362215179:web:9a28af932292d45d9f0c0e",
+    measurementId: "G-80XSTM8F3N"
 };
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
 function signupUser() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    createUserWithEmailAndPassword(auth, email, password)
+    auth.createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
             alert("Signup successful! Welcome, " + userCredential.user.email);
         })
@@ -25,7 +23,7 @@ function signupUser() {
 function loginUser() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    signInWithEmailAndPassword(auth, email, password)
+    auth.signInWithEmailAndPassword(email, password)
         .then((userCredential) => {
             alert("Login successful! Welcome back, " + userCredential.user.email);
         })
@@ -34,7 +32,7 @@ function loginUser() {
         });
 }
 function logoutUser() {
-    signOut(auth)
+    auth.signOut()
         .then(() => {
             alert("You have logged out.");
             location.href = "index.html";
@@ -43,3 +41,6 @@ function logoutUser() {
             alert("Error: " + error.message);
         });
 }
+window.signupUser = signupUser;
+window.loginUser = loginUser;
+window.logoutUser = logoutUser;
