3ee785f02363dc5ce5004f418f66327ea865bf76
Ashirvaad Verma <ashirvaadverma2000@zohomail.in>
2024-12-03 13:29:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
 import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 import { getFirestore, doc, setDoc, getDocs, collection } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyADJuT7UDOxpu4gaV6aRpOCzsBPBb0O368",
@@ -134,6 +135,10 @@ async function displayProducts() {
 if (location.pathname.includes('view-products.html')) {
     window.onload = displayProducts;
 }
+
+const supabaseUrl = "https://izvdtpdjsnwerogmrwiv.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6dmR0cGRqc253ZXJvZ21yd2l2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMxNjIwNDMsImV4cCI6MjA0ODczODA0M30.2PlstYR2Bg8tZnwcgKj8nblnpHLNZQIXAon8kBx90Yw";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 window.addProduct = addProduct;
 window.signupUser = signupUser;
